58d5ff5450377e97b0dca0fc7cfc5f01335d243c
root <Nex Zhu>
2023-10-14 18:37:35 +0800

    style: lint:fix

--- viteConfig.ts
+++ viteConfig.ts
@@ -1,26 +1,24 @@
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 
+import VueI18n from '@intlify/unplugin-vue-i18n/vite'
+import LinkAttributes from 'markdown-it-link-attributes'
+import Shiki from 'markdown-it-shikiji'
+import Unocss from 'unocss/vite'
 // import Preview from 'vite-plugin-vue-component-preview'
 // import Vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-
+import Markdown from 'unplugin-vue-markdown/vite'
 // import VueMacros from 'unplugin-vue-macros/vite'
 import Inspect from 'vite-plugin-inspect'
 import Pages from 'vite-plugin-pages'
-import Inspector from 'vite-plugin-vue-inspector'
 import { VitePWA } from 'vite-plugin-pwa'
-import VueI18n from '@intlify/unplugin-vue-i18n/vite'
 import VueDevTools from 'vite-plugin-vue-devtools'
-import LinkAttributes from 'markdown-it-link-attributes'
-import Unocss from 'unocss/vite'
-import Shiki from 'markdown-it-shikiji'
-
+import Inspector from 'vite-plugin-vue-inspector'
+import Layouts from 'vite-plugin-vue-layouts'
 // import VueMacros from 'unplugin-vue-macros/vite'
 import WebfontDownload from 'vite-plugin-webfont-dl'
-import Layouts from 'vite-plugin-vue-layouts'
-import Markdown from 'unplugin-vue-markdown/vite'
 import generateSitemap from 'vite-ssg-sitemap'
 
 const __filename = fileURLToPath(import.meta.url)
