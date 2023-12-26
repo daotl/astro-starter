f5a14c39a18967b0038afcfd1625947cdfaf0d40
lifang <2393477099@qq.com>
2023-12-26 15:21:55 +0800

    feat: scrolltrigger animation

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -9,8 +9,13 @@ declare module 'vue' {
   export interface GlobalComponents {
     Components: typeof import('./../components/index.vue')['default']
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElMenu: typeof import('element-plus/es')['ElMenu']
+    ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
+    ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
+    Header: typeof import('../components/Menu.vue')['default']
     HiPage: typeof import('./../components/HiPage.vue')['default']
     Home: typeof import('./../components/Home.vue')['default']
+    Menu: typeof import('./../components/Menu.vue')['default']
     README: typeof import('./../components/README.md')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
