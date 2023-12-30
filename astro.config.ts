83d68fb8606316c6da8d326db210866436242970
lifang <2393477099@qq.com>
2023-12-30 15:39:16 +0800

    feat: add @astrojs/node

--- astro.config.ts
+++ astro.config.ts
@@ -1,3 +1,4 @@
+import node from '@astrojs/node'
 import vue from '@astrojs/vue'
 import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
@@ -7,15 +8,18 @@ import vite from './viteConfig'
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
-  integrations: [
-    vue({
-      appEntrypoint: '/src/vue',
-      jsx: true,
-    }),
-    UnoCSS({ injectReset: true }),
-  ],
+  integrations: [vue({
+    appEntrypoint: '/src/vue',
+    jsx: true,
+  }), UnoCSS({
+    injectReset: true,
+  })],
   // Temporarily removed for causing error:
   //   error   Failed to parse source for import analysis because the content contains invalid JS syntax. You may need to install appropriate plugins to handle the .md file format, or if it's an asset, add "**/*.md" to `assetsInclude` in your configuration.
   // See: https://github.com/withastro/astro/issues/5265
   vite,
+  output: 'hybrid',
+  adapter: node({
+    mode: 'standalone',
+  }),
 })
